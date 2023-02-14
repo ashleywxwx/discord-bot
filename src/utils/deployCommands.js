f25a68c04522138479f250249d7fd002419957ab
Ashley Weatherwax <ashley.wxwx@gmail.com>
2023-02-14 17:41:57 -0600

    Some cleanup and organization

--- src/utils/deployCommands.js
+++ src/utils/deployCommands.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
